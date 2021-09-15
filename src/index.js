767260a6114526c4f39e144ac33040f7cdbbb735
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2021-09-15 20:20:20 -0300

    Added Settings

--- src/index.js
+++ src/index.js
@@ -1,73 +1,204 @@
 /* globals chrome */
 
-const ethProvider = require('eth-provider')
-const provider = ethProvider('ws://127.0.0.1:6969?identity=avme-plugin')
-const subs = {}
-const pending = {}
+/// EXTENSION ENTRY POINT
 
-const getOrigin = url => {
-  const path = url.split('/')
-  return path[0] + '//' + path[2]
-}
 
-const pop = show => {
-  if (show) {
-    chrome.browserAction.setPopup({ popup: 'pop.html' })
-  } else {
-    chrome.browserAction.setPopup({ popup: 'settings.html' })
-  }
-}
+/*User settings*/
 
-pop(true)
-provider.on('connect', () => pop(false))
-provider.on('disconnect', () => pop(true))
+const init = new Promise(async (resolve) => {
+    let address;
+    let port;
+    /// ADDRESS
+    await chrome.storage.sync.get(['address'], async (result) => {
 
-provider.connection.on('payload', payload => {
-  console.log(payload);
-  if (typeof payload.id !== 'undefined') {
-    if (pending[payload.id]) {
-      const { tabId, payloadId } = pending[payload.id]
-      if (pending[payload.id].method === 'eth_subscribe' && payload.result) {
-        subs[payload.result] = { tabId, send: subload => chrome.tabs.sendMessage(tabId, subload) }
-      } else if (pending[payload.id].method === 'eth_unsubscribe') {
-        const params = payload.params ? [].concat(payload.params) : []
-        params.forEach(sub => delete subs[sub])
-      }
-      chrome.tabs.sendMessage(tabId, Object.assign({}, payload, { id: payloadId }))
-      delete pending[payload.id]
+        console.log(`result ${result.address}`)
+        if(result.address === undefined)
+        {
+            address = '127.0.0.1';
+            chrome.storage.sync.set({address});
+        }
+        else address = result.address;
+
+        console.log(`final param ${address}`);
+    });
+
+    /// PORT
+    await chrome.storage.sync.get(['port'], async (result) => {
+
+    console.log(`result ${result.port}`)
+    if(result.port === undefined)
+    {
+        port = 6969;
+        chrome.storage.sync.set({port});
     }
-  } else if (payload.method && payload.method.indexOf('_subscription') > -1 && subs[payload.params.subscription]) { // Emit subscription result to tab
-    subs[payload.params.subscription].send(payload)
-  }
+    else port = result.port;
+
+    console.log(`final param ${port}`);
+
+    resolve({address,port});
+    });
 })
 
-chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => {
-  if (payload.method === 'wallet_call') return provider.connection.send(payload)
-  const id = provider.nextId++
-  pending[id] = { tabId: sender.tab.id, payloadId: payload.id, method: payload.method }
-  const load = Object.assign({}, payload, { id, __frameOrigin: getOrigin(sender.url) })
-  provider.connection.send(load)
-})
+init.then((url) => {
+    startProviders(url);
+});
 
-// chrome.browserAction.onClicked.addListener(tab => {
-//   if (provider.connected) {
-//     const load = { jsonrpc: '2.0', id: 1, method: 'wallet_call', params: [] }
-//     provider.connection.send(load)
-//   } else {
-//     if (provider && provider.close) provider.close()
-//     provider = ethProvider('ws://127.0.0.1:1248?identity=avme-plugin')
-//   }
-// })
 
-const unsubscribeTab = tabId => {
-  Object.keys(pending).forEach(id => { if (pending[id].tabId === tabId) delete pending[id] })
-  Object.keys(subs).forEach(sub => {
-    if (subs[sub].tabId === tabId) {
-      provider.send({ jsonrpc: '2.0', id: 1, method: 'eth_unsubscribe', params: [sub] })
-      delete subs[sub]
+const startProviders = url => {
+    let {address, port} = url;
+    console.info(`STARTING WS CLIENT AT: "ws://${address}:${port}?identity=avme-plugin"`);
+    const ethProvider = require('eth-provider');
+    const provider = ethProvider(`ws://${address}:${port}?identity=avme-plugin`);
+    const subs = {};
+    const pending = {};
+
+    const getOrigin = url => {
+        const path = url.split('/')
+        return path[0] + '//' + path[2]
     }
-  })
+
+    const pop = show => {
+        if (show) chrome.browserAction.setPopup({ popup: 'pop.html' });
+        else chrome.browserAction.setPopup({ popup: 'settings.html' });
+    }
+    
+    pop(true);
+
+    provider.on('connect', () => pop(false));
+    provider.on('disconnect', () => pop(true));
+
+    provider.connection.on('payload', payload => {
+        console.log(payload);
+        if (typeof payload.id !== 'undefined') {
+            if (pending[payload.id]) {
+                const { tabId, payloadId } = pending[payload.id]
+                if (pending[payload.id].method === 'eth_subscribe' && payload.result) {
+                    subs[payload.result] = { tabId, send: subload => chrome.tabs.sendMessage(tabId, subload) }
+                }
+                else if (pending[payload.id].method === 'eth_unsubscribe') {
+                    const params = payload.params ? [].concat(payload.params) : []
+                    params.forEach(sub => delete subs[sub])
+                }
+                chrome.tabs.sendMessage(tabId, Object.assign({}, payload, { id: payloadId }))
+                delete pending[payload.id]
+            }
+        }
+        else if (payload.method && payload.method.indexOf('_subscription') > -1 && subs[payload.params.subscription])
+        {   // Emit subscription result to tab
+            subs[payload.params.subscription].send(payload)
+        }
+    })
+
+    chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => {
+        // console.log('ESSE FDP TA DISPARANDO MAIS REQUEST');
+        if (payload.method === 'wallet_call') return provider.connection.send(payload)
+        const id = provider.nextId++
+        pending[id] = { tabId: sender.tab.id, payloadId: payload.id, method: payload.method }
+        const load = Object.assign({}, payload, { id, __frameOrigin: getOrigin(sender.url) })
+        provider.connection.send(load)
+    })
+
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+            console.log(
+                `Storage key "${key}" in namespace "${namespace}" changed.`,
+                `Old value was "${oldValue}", new value is "${newValue}".`
+            );
+            if(key === 'address') address = newValue;
+            if(key === 'port') port = newValue;
+        }    
+        //   provider.close();
+        // console.log(provider);
+        // if(provider.isConnected())
+        // {
+        //     console.info(`Closing Connection to... ${address}:${url}`);
+        //     provider.close();
+        // }
+        provider.connection.close();
+        startProviders({address,port});
+    });
 }
 
-chrome.tabs.onRemoved.addListener((tabId, removed) => unsubscribeTab(tabId))
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => { if (changeInfo.url) unsubscribeTab(tabId) })
+
+    // const ethProvider = require('eth-provider');
+
+    // const getOrigin = url => {
+    //     const path = url.split('/')
+    //     return path[0] + '//' + path[2]
+    // }
+
+    // const pop = show => {
+    //     if (show) chrome.browserAction.setPopup({ popup: 'pop.html' });
+    //     else chrome.browserAction.setPopup({ popup: 'settings.html' });
+    // }
+
+    // const url = `ws://${address}:${port}?identity=avme-plugin`;
+    // console.log(url);
+    // const provider = ethProvider(`ws://${address}:${port}?identity=avme-plugin`);
+    // // const provider = ethProvider(`ws://127.0.0.1:6969?identity=avme-plugin`);
+    // const subs = {};
+    // const pending = {};
+
+    // pop(true);
+    // provider.on('connect', () => pop(false));
+    // provider.on('disconnect', () => pop(true));
+
+    // provider.connection.on('payload', payload => {
+    //     console.log(payload);
+    //     if (typeof payload.id !== 'undefined') {
+    //         if (pending[payload.id]) {
+    //             const { tabId, payloadId } = pending[payload.id]
+    //             if (pending[payload.id].method === 'eth_subscribe' && payload.result) {
+    //                 subs[payload.result] = { tabId, send: subload => chrome.tabs.sendMessage(tabId, subload) }
+    //             }
+    //             else if (pending[payload.id].method === 'eth_unsubscribe') {
+    //                 const params = payload.params ? [].concat(payload.params) : []
+    //                 params.forEach(sub => delete subs[sub])
+    //             }
+    //             chrome.tabs.sendMessage(tabId, Object.assign({}, payload, { id: payloadId }))
+    //             delete pending[payload.id]
+    //         }
+    //     }
+    //     else if (payload.method && payload.method.indexOf('_subscription') > -1 && subs[payload.params.subscription])
+    //     {   // Emit subscription result to tab
+    //         subs[payload.params.subscription].send(payload)
+    //     }
+    // })
+
+    // chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => {
+    //     if (payload.method === 'wallet_call') return provider.connection.send(payload)
+    //     const id = provider.nextId++
+    //     pending[id] = { tabId: sender.tab.id, payloadId: payload.id, method: payload.method }
+    //     const load = Object.assign({}, payload, { id, __frameOrigin: getOrigin(sender.url) })
+    //     provider.connection.send(load)
+    // })
+
+
+
+
+
+
+    // chrome.browserAction.onClicked.addListener(tab => {
+    //   if (provider.connected) {
+    //     const load = { jsonrpc: '2.0', id: 1, method: 'wallet_call', params: [] }
+    //     provider.connection.send(load)
+    //   } else {
+    //     if (provider && provider.close) provider.close()
+    //     provider = ethProvider('ws://127.0.0.1:1248?identity=avme-plugin')
+    //   }
+    // })
+
+    /*
+    const unsubscribeTab = tabId => {
+        Object.keys(pending).forEach(id => { if (pending[id].tabId === tabId) delete pending[id] })
+        Object.keys(subs).forEach(sub => {
+        if (subs[sub].tabId === tabId) {
+            provider.send({ jsonrpc: '2.0', id: 1, method: 'eth_unsubscribe', params: [sub] })
+            delete subs[sub]
+        }
+        })
+    }
+
+    chrome.tabs.onRemoved.addListener((tabId, removed) => unsubscribeTab(tabId))
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => { if (changeInfo.url) unsubscribeTab(tabId) })
+    */
---
+++ src/popup.js
@@ -0,0 +1,21 @@
+const dom = document;
+const _find = document.querySelector.bind(dom);
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    _find('#options-popup').addEventListener("click", async () => {
+        _find('.options').classList.remove('disabled');
+        await chrome.storage.sync.get(['address'], async ({address}) => {
+            _find('#address').value = address;
+        });
+        await chrome.storage.sync.get(['port'], async ({port}) => {
+            _find('#port').value = port;
+        });
+    });
+        
+    _find('#close-options').addEventListener("click", () => {
+        _find('.options').classList.add('disabled');
+        chrome.storage.sync.set({'address':_find('#address').value});
+        chrome.storage.sync.set({'port':_find('#port').value});
+    });
+});
