aa0dd3d79912d04b6488c0df5e89df4913735323
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2021-10-14 16:43:02 -0300

    Removed escape/unescape in "inline.js" and generated code "injected.js"

--- src/extension.js
+++ src/extension.js
@@ -1,57 +1,57 @@
-const EventEmitter = require('events');
-const EthereumProvider = require('ethereum-provider');
-const detectEthereumProvider = require('@metamask/detect-provider');
+const EventEmitter = require('events')
+const EthereumProvider = require('ethereum-provider')
+const detectEthereumProvider = require('@metamask/detect-provider')
 
 class Connection extends EventEmitter {
   constructor () {
-    super();
+    super()
     window.addEventListener('message', event => {
       if (event && event.source === window && event.data && event.data.type === 'eth:payload') {
         this.emit('payload', event.data.payload)
       }
     })
-    setTimeout(() => this.emit('connect'), 0);
+    setTimeout(() => this.emit('connect'), 0)
   }
 
   send (payload) {
-    window.postMessage({ type: 'eth:send', payload }, window.location.origin);
+    window.postMessage({ type: 'eth:send', payload }, window.location.origin)
   }
 }
 
-let extensionEnabled = window.localStorage.getItem('__avmePlugin__');
-let isConnected = window.localStorage.getItem('__isConnected__');
+let extensionEnabled = window.localStorage.getItem('__avmePlugin__')
+let isConnected = window.localStorage.getItem('__isConnected__')
 
 try {
-  extensionEnabled = JSON.parse(extensionEnabled);
+  extensionEnabled = JSON.parse(extensionEnabled)
 } catch (e) {
-  extensionEnabled = false;
+  extensionEnabled = false
 }
 
 try {
-  isConnected = JSON.parse(isConnected);
+  isConnected = JSON.parse(isConnected)
 } catch (e) {
-  isConnected = false;
-  window.localStorage.setItem('__isConnected__', JSON.stringify(false));
+  isConnected = false
+  window.localStorage.setItem('__isConnected__', JSON.stringify(false))
 }
 
 (async () =>{
   class MetaMaskProvider extends EthereumProvider {}
   try
   {
-    const detected = await detectEthereumProvider({timeout:250});
-    console.log(detected);
+    const detected = await detectEthereumProvider({timeout:250})
+    console.log(detected)
     if(detected)
     {
-      console.info("Metamask was detected");
+      console.info("Metamask was detected")
     }
     
     if (extensionEnabled && isConnected) {
       try {
-        window.ethereum = new MetaMaskProvider(new Connection());
-        window.ethereum.isAvmeExtension = true;
-        window.ethereum.isMetaMask = true;
-        window.ethereum._metamask = true;
-        window.ethereum.setMaxListeners(0);
+        window.ethereum = new MetaMaskProvider(new Connection())
+        window.ethereum.isAvmeExtension = true
+        window.ethereum.isMetaMask = true
+        window.ethereum._metamask = true
+        window.ethereum.setMaxListeners(0)
       } catch (e) {
         console.error('Error:', e)
       }
@@ -60,14 +60,14 @@ try {
   {
     if (extensionEnabled && isConnected) {
       try {
-        window.ethereum = new MetaMaskProvider(new Connection());
-        window.ethereum.isAvmeExtension = true;
-        window.ethereum.isMetaMask = true;
-        window.ethereum._metamask = true;
-        window.ethereum.setMaxListeners(0);
+        window.ethereum = new MetaMaskProvider(new Connection())
+        window.ethereum.isAvmeExtension = true
+        window.ethereum.isMetaMask = true
+        window.ethereum._metamask = true
+        window.ethereum.setMaxListeners(0)
       } catch (e) {
         console.error('Error:', e)
       }
     }
   }
-})();
+})()
--- src/inline.js
+++ src/inline.js
@@ -2,8 +2,24 @@ const fs = require('fs')
 const path = require('path')
 const ncp = require('ncp')
 
+// const inject = `
+//   var extension = unescape('${escape(fs.readFileSync(path.join(__dirname, '../dist/extension.js')).toString())}')
+//   try {
+//     chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => window.postMessage({type: 'eth:payload', payload: payload}, window.location.origin))
+//     window.addEventListener('message', event => {
+//       if (event.source === window && event.data && event.data.type === 'eth:send') chrome.runtime.sendMessage(event.data.payload)
+//     })
+//     let script = document.createElement('script')
+//     script.setAttribute('type', 'text/javascript')
+//     script.innerText = extension
+//     script.onload = function () { this.remove() }
+//     document.head ? document.head.prepend(script) : document.documentElement.prepend(script)
+//   } catch (e) {
+//     console.log(e)
+//   }
+// `
 const inject = `
-  var extension = unescape('${escape(fs.readFileSync(path.join(__dirname, '../dist/extension.js')).toString())}')
+  var extension = String.raw\`${fs.readFileSync(path.join(__dirname, "../dist/extension.js")).toString()}\`
   try {
     chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => window.postMessage({type: 'eth:payload', payload: payload}, window.location.origin))
     window.addEventListener('message', event => {
